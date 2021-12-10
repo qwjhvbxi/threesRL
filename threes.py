914415511d246cbd897873add55536171915f851
qwjhvbxi <iacobucci.riccardo@gmail.com>
2021-12-10 14:00:08 +0100

    Initial commit

---
+++ threes.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 11:45:36 2021
+
+@author: iacob
+"""
+import warnings
+import numpy as np
+
+warnings.filterwarnings(action='ignore', message='All-NaN slice encountered')
+
+
+def encodeboard(board):
+    board0 = np.zeros([15,4,4],dtype=int);
+    def_numbers = np.concatenate(([1,2],np.exp2(np.arange(13))*3));
+    for i in range(0,def_numbers.size):
+        board0[i,:,:]=(board==def_numbers[i]).astype(int);
+    return board0;
+    
+def decodeboard(board,rang=np.arange(15)):
+    def_numbers = np.concatenate(([1,2],np.exp2(np.arange(13))*3));
+    outboard = np.matmul(np.moveaxis(board[rang,:,:],0,2),np.transpose(def_numbers[rang]));
+    return outboard.astype(int);
+
+def movelr(board):
+    
+    helpermat = np.array([[1,2,3],[1,2,3],[1,2,3],[1,2,3]]);
+    
+    # moving
+    #mover = [[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,1]];
+    b = np.sum(board,0);
+    # movinglines = np.matmul(b,mover);
+    # movablelines = (np.max(movinglines,1)==1);
+    
+    optmovesmat=b[:,0:3]-b[:,1:4];
+    optmovesL=optmovesmat<0;
+    optmovesR=optmovesmat>0;
+    
+    boardnum = decodeboard(board);
+    optpowersmat = decodeboard(board,np.arange(2,15));
+    optthreesmat = decodeboard(board,np.arange(0,2));
+    
+    optpowersmat = np.where(optpowersmat==0,np.nan,optpowersmat);
+    optpowers = (optpowersmat[:,0:3]-optpowersmat[:,1:4]==0);
+    
+    optthrees=optthreesmat[:,0:3]+optthreesmat[:,1:4]==3;
+    
+    optcombiL = (optthrees+optpowers+optmovesL)*helpermat;
+    optcombiR = (optthrees+optpowers+optmovesR)*helpermat;
+    
+    optcombiL = np.where(optcombiL==0,np.nan,optcombiL);
+    optcombiR = np.where(optcombiR==0,np.nan,optcombiR);
+    
+    activelinesL = np.nansum(optcombiL,1)>0;
+    activelinesR = np.nansum(optcombiR,1)>0;
+    
+    movementL = np.nanmin(optcombiL,1).astype(int)-1;
+    movementR = np.nanmax(optcombiR,1).astype(int)-1;
+    
+    newbL = np.copy(boardnum);
+    newbR = np.copy(boardnum);
+    
+    for i in range(0,4):
+        if activelinesL[i]:
+            newbL[i,movementL[i]:3] = boardnum[i,movementL[i]+1:];
+            newbL[i,movementL[i]] = boardnum[i,movementL[i]] + boardnum[i,movementL[i]+1];
+            newbL[i,3]=0;
+        if activelinesR[i]:
+            newbR[i,1:movementR[i]+1] = boardnum[i,0:movementR[i]];
+            newbR[i,movementR[i]+1] = boardnum[i,movementR[i]] + boardnum[i,movementR[i]+1];
+            newbR[i,0]=0;
+            
+    return (activelinesL,activelinesR,newbL,newbR); 
+
+
+class tre:
+    
+    def_numbers = np.concatenate(([1,2],np.exp2(np.arange(13))*3));
+    
+    def __init__(self):
+        self.reset();
+        
+    def maxnum(self):
+        maxnum_index = np.where(np.sum(self.board,(1,2)))[0].max();
+        return self.def_numbers[maxnum_index];
+        
+        
+    def state(self):
+        nextpiecevec = np.zeros((10,1));
+        nextpiecevec[self.nextpiece-1] = 1;
+        return np.append(self.board.flatten(),nextpiecevec);
+        
+    
+    def reset(self):
+        # 1 2 3 6 12 24 48 96 192 384 768 1500 3000 6000 12000 -> 15
+        # 4x4x15 planes
+        self.nextpiece = np.random.randint(1,4);
+        board = np.zeros([15,4,4],dtype=int);
+        
+        k = np.random.choice((-1,0,1,2),size=(4,4),p=(0.5,0.2,0.2,0.1));
+        for i in range(0,4):
+            for j in range(0,4):
+                if k[i,j]>=0:
+                    board[k[i,j],i,j]=1;
+                    
+        # board[0,0,3]=1;
+        # board[0,0,2]=1;
+        # board[1,0,1]=1;
+        # board[3,2,1]=1;
+        # board[3,2,2]=1;
+        self.board = board;
+        return self.state();
+    
+    def step(self,action):
+        moved,done = self.nextmove(action);
+        # reward = int(moved)*1.1 - 0.1;
+        reward = int(moved)*2 - 1;
+        return (self.state(),reward,done);
+    
+    def nextmove(self,move): # 0,1,2,3 = L,R,U,D
+        
+        activelines,newb = self.possiblemoves();
+        
+        numactivelinestotal = np.sum(activelines);
+        numactivelines = np.sum(activelines[move]);
+        done = False;
+        moved = True;
+        
+        if numactivelinestotal==0: # lost
+            done = True;
+            moved = False;
+        elif numactivelines==0: # not lost, but move is not allowed
+            moved = False;
+        else:
+            nextpiece_pos = np.random.choice(4,1,p=activelines[move]/numactivelines);
+            
+            if move==0:
+                 newb[move,nextpiece_pos,3] = self.nextpiece;
+            if move==1:
+                 newb[move,nextpiece_pos,0] = self.nextpiece;
+            if move==2:
+                 newb[move,3,nextpiece_pos] = self.nextpiece;
+            if move==3:
+                 newb[move,0,nextpiece_pos] = self.nextpiece;
+            
+            self.board = encodeboard(newb[move,:,:]);
+            self.nextpiece = np.random.randint(1,4);
+            
+        return (moved,done)
+    
+    
+    def possiblemoves(self): #this refers to only one direction, can be called multiple times for the 4 moves
+        
+        activelinesL,activelinesR,newbL,newbR = movelr(self.board);
+        activelinesU,activelinesD,newbU,newbD = movelr(np.transpose(self.board,(0,2,1)));
+        
+        newbU=np.transpose(newbU);
+        newbD=np.transpose(newbD);
+        
+        activelines=np.stack((activelinesL,activelinesR,activelinesU,activelinesD));
+        newb=np.stack((newbL,newbR,newbU,newbD));
+        
+        return (activelines,newb);
+        
+    def decode(self,rang=np.arange(15)):
+        outboard = decodeboard(self.board,rang);
+        return outboard.astype(int);
+        
+    def show(self):
+        outboard = self.decode();
+        print(outboard);
+    
+
---
+++ training.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 11:33:20 2021
+
+@author: iacob
+"""
+
+import threes as game
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+
+# Configuration parameters for the whole setup
+gamma = 0.99  # Discount factor for past rewards
+max_steps_per_episode = 100;
+eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
+
+env = game.tre();
+
+num_inputs = 250 # 4*4*15 (board) + 10 (next piece: 1,2,3,6,6-12,12-96,...)
+num_actions = 4
+# num_hidden1 = 250
+# num_hidden2 = 125
+num_hidden1 = 125
+num_hidden2 = 125
+# num_hidden2 = 250
+
+inputs = layers.Input(shape=(num_inputs,))
+common1 = layers.Dense(num_hidden1, activation="relu")(inputs)
+common2 = layers.Dense(num_hidden2, activation="relu")(common1)
+action = layers.Dense(num_actions, activation="softmax")(common2)
+critic = layers.Dense(1)(common2)
+
+model = keras.Model(inputs=inputs, outputs=[action, critic])
+
+optimizer = keras.optimizers.Adam(learning_rate=0.0001,clipnorm=1)
+huber_loss = keras.losses.Huber()
+action_probs_history = []
+critic_value_history = []
+rewards_history = []
+running_reward = 0
+episode_count = 0
+maxnum = 0;
+reward_progress = [];
+
+while True:  # Run until solved
+    state = env.reset();
+    episode_reward = 0
+    with tf.GradientTape() as tape:
+        for timestep in range(1, max_steps_per_episode):
+        # while True: # run until lost
+            
+            state = tf.convert_to_tensor(state)
+            state = tf.expand_dims(state, 0)
+
+            action_probs, critic_value = model(state)
+            critic_value_history.append(critic_value[0, 0])
+            
+            activelines,_ = env.possiblemoves();
+            possiblemoves = np.sum(activelines,1)>0;
+            
+            if np.sum(possiblemoves)==0:
+                break
+            
+            else:
+                
+                # set illegal moves to 0 and renormalize
+                probs = np.squeeze(action_probs);
+                # probs = probs*possiblemoves + 0.01;
+                # probs = probs/np.sum(probs);
+    
+                # Sample action from action probability distribution
+                action = np.random.choice(num_actions, p=probs)
+                action_probs_history.append(tf.math.log(action_probs[0, action]))
+    
+                # Apply the sampled action in our environment
+                state, reward, done = env.step(action)
+                rewards_history.append(reward)
+                episode_reward += reward
+            
+            # if done:
+            #     break
+            
+        # Update running reward to check condition for solving
+        running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
+        
+        maxnum = np.max((maxnum,env.maxnum())).astype(int);
+
+        # Calculate expected value from rewards
+        # - At each timestep what was the total reward received after that timestep
+        # - Rewards in the past are discounted by multiplying them with gamma
+        # - These are the labels for our critic
+        returns = []
+        discounted_sum = 0
+        for r in rewards_history[::-1]:
+            discounted_sum = r + gamma * discounted_sum
+            returns.insert(0, discounted_sum)
+
+        # Normalize
+        returns = np.array(returns)
+        returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
+        returns = returns.tolist()
+
+        # Calculating loss values to update our network
+        history = zip(action_probs_history, critic_value_history, returns)
+        actor_losses = []
+        critic_losses = []
+        for log_prob, value, ret in history:
+            # At this point in history, the critic estimated that we would get a
+            # total reward = `value` in the future. We took an action with log probability
+            # of `log_prob` and ended up recieving a total reward = `ret`.
+            # The actor must be updated so that it predicts an action that leads to
+            # high rewards (compared to critic's estimate) with high probability.
+            diff = ret - value
+            actor_losses.append(-log_prob * diff)  # actor loss
+
+            # The critic must be updated so that it predicts a better estimate of
+            # the future rewards.
+            critic_losses.append(
+                huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+            )
+
+        # Backpropagation
+        loss_value = sum(actor_losses) + sum(critic_losses)
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        # Clear the loss and reward history
+        action_probs_history.clear()
+        critic_value_history.clear()
+        rewards_history.clear()
+        
+        reward_progress.append(running_reward);
+
+    # Log details
+    episode_count += 1
+    if episode_count % 10 == 0:
+        template = "running reward: {:.2f} at episode {}. Max number: {}"
+        print(template.format(running_reward, episode_count, maxnum))
+        maxnum = 0;
+        
+    if episode_count % 100 == 0:
+        plt.plot(reward_progress)
+
+    if running_reward > 195:  # Condition to consider the task solved
+        print("Solved at episode {}!".format(episode_count))
+        break
+    
+plt.plot(reward_progress)
+
+# plot
+state = env.reset()
+action_history = [];
+for timestep in range(1, max_steps_per_episode):
+    
+    #predictions = model.predict(state)
+    
+    env.show();
+    state = tf.convert_to_tensor(state)
+    state = tf.expand_dims(state, 0)
+
+    action_probs, critic_value = model(state)
+    
+    print(action_probs)
+
+    # Sample action from action probability distribution
+    action = np.argmax(np.squeeze(action_probs))
+    
+    action_history.append(action);
+
+    # Apply the sampled action in our environment
+    state, reward, done = env.step(action)
+    
+    if done:
+        break
+    
+
